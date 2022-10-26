954f1453bedbb06e4a5eeeb236ec7a734b1febd3
Kalev Päädam <kalevai@gmail.com>
2022-10-26 23:30:04 +0200

    Removed event viewer reader. Added memory use check. Added usersOnline check. Server will restart after 24 hours if no players are online.

--- Program.cs
+++ Program.cs
@@ -11,37 +11,37 @@ namespace minecraft_restarter
 {
     internal static class Program
     {
-        private static int lineCount = 0;
-        private static StringBuilder output = new StringBuilder();
+        private static string lastLine = "";
         private static DateTime timeOfLastRestart = DateTime.UtcNow;
-        private static int memsize = 0; // memsize in KB
         public static void Main()
         {
-            string command = "";
+
             Process ServerProc = new Process();
             ServerStart(ServerProc);
-            _ = CheckMemoryUse(ServerProc);
 
             while (true)
             {
 
                 bool serverUp = IsServer_up();
-                bool outOfMemory = CheckOutOfMemory();
 
-                if (outOfMemory)
+                if (CheckMemoryUse(ServerProc) / 1024 > 6870)
                 {
                     ServerStop(ServerProc);
-                    break;
+                    ServerStart(ServerProc);
+                }
+
+                if (ServerUptime().TotalHours > 24 && PlayersOnline(ServerProc))
+                {
+                    ServerStop(ServerProc);
+                    ServerStart(ServerProc);
                 }
 
                 if (serverUp)
                 {
                     Console.WriteLine("server up since " + timeOfLastRestart + "UTC");
-                    Console.WriteLine("Memory used " + (memsize / 1024) + "MB");
-                    Console.WriteLine("Uptime is " + (DateTime.UtcNow - timeOfLastRestart));
-                    ///Thread.Sleep(5000);
-                    AskForExit(ServerProc);
-                    ServerOutput(ServerProc);
+                    Console.WriteLine("Memory used " + (CheckMemoryUse(ServerProc) / 1024) + "MB");
+                    Console.WriteLine("Uptime is " + (ServerUptime()));
+                    AskForCommands(ServerProc);
                 }
                 else
                 {
@@ -52,30 +52,35 @@ namespace minecraft_restarter
             }
         }
 
-        private static void AskForExit(Process ServerProc)
+        private static TimeSpan ServerUptime()
+        {
+            TimeSpan uptime = DateTime.UtcNow.Subtract(timeOfLastRestart);
+            return uptime;
+        }
+
+        private static void AskForCommands(Process ServerProc)
         {
             try
             {
                 Console.WriteLine("write stop in the next 10 seconds to stop the server");
-                string command = Reader.ReadLine(10000);
+                string command = Reader.ReadLine(15000);
                 if (command == "stop")
                 {
                     ServerStop(ServerProc);
                     Environment.Exit(0);
                 }
+                if (command == "restart")
+                {
+                    ServerStop(ServerProc);
+                    ServerStart(ServerProc);
+                }
+
+                ServerCommandInputWriter(ServerProc, command);
             }
             catch (TimeoutException)
             {
-                //Console.WriteLine("Sorry, you waited too long.");
             }
-            return;
         }
-
-        private static void ServerOutput(Process ServerProc)
-        {
-
-        }
-
 
         private static void ServerStart(Process ServerProc)
         {
@@ -93,6 +98,7 @@ namespace minecraft_restarter
             startInfo.RedirectStandardInput = true;
             startInfo.UseShellExecute = false; // Necessary for Standard Stream Redirection
             startInfo.CreateNoWindow = true; // You can do either this or open it with "javaw" instead of "java"
+
             ServerProc.EnableRaisingEvents = true;
             ServerProc.StartInfo = startInfo;
             ServerProc.OutputDataReceived += new DataReceivedEventHandler(ServerOutputDataReceived);
@@ -100,10 +106,9 @@ namespace minecraft_restarter
             ServerProc.Start();
 
             ServerProc.BeginOutputReadLine();
-            ServerProc.WaitForExit();
 
             timeOfLastRestart = DateTime.UtcNow;
-            Thread.Sleep(10000);
+            Thread.Sleep(15000);
 
         }
         static void ServerErrorDataReceived(object sender, DataReceivedEventArgs e)
@@ -114,6 +119,7 @@ namespace minecraft_restarter
         static void ServerOutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine("{0}", e.Data);
+            lastLine = e.Data;
         }
 
         private static void ServerStop(Process ServerProc)
@@ -135,7 +141,24 @@ namespace minecraft_restarter
             }
         }
 
-        private static string Countdown(StreamWriter myStreamWriter)
+        private static Boolean PlayersOnline(Process ServerProc)
+        {
+            StreamWriter myStreamWriter = ServerProc.StandardInput;
+            String inputText;
+            string checkFor = "There are 0";
+            inputText = "list";
+            myStreamWriter.WriteLine(inputText);
+            bool b = lastLine.Contains(checkFor);
+            return b;
+        }
+
+        private static void ServerCommandInputWriter(Process ServerProc, string command)
+        {
+            StreamWriter myStreamWriter = ServerProc.StandardInput;
+            myStreamWriter.WriteLine(command);
+        }
+
+        private static void Countdown(StreamWriter myStreamWriter)
         {
             string inputText = "say server restart in 1 minute";
             myStreamWriter.WriteLine(inputText);
@@ -147,14 +170,13 @@ namespace minecraft_restarter
             myStreamWriter.WriteLine(inputText);
             Thread.Sleep(15000);
             inputText = "say 15 seconds left, THIS IS NOT A DRILL";
+            myStreamWriter.WriteLine(inputText);
             Thread.Sleep(5000);
             for (int a = 10; a >= 0; a--)
             {
                 myStreamWriter.WriteLine("say server restart in {0}", a);
                 Thread.Sleep(1000);
             }
-
-            return inputText;
         }
 
         public static int CheckMemoryUse(Process ServerProc)
@@ -164,54 +186,10 @@ namespace minecraft_restarter
             PC.CategoryName = "Process";
             PC.CounterName = "Working Set - Private";
             PC.InstanceName = proc.ProcessName;
-            memsize = Convert.ToInt32(PC.NextValue()) / (int)(1024);
+            int memsize = Convert.ToInt32(PC.NextValue()) / (int)(1024);
             PC.Close();
             PC.Dispose();
             return memsize;
-        }
-
-
-        public static bool CheckOutOfMemory()
-        {
-
-            String myEventType = null;
-            // Associate the instance of 'EventLog' with local System Log.
-            EventLog myEventLog = new EventLog("System", ".");
-            int myOption = Convert.ToInt32(3);
-            switch (myOption)
-            {
-                case 1:
-                    myEventType = "Error";
-                    break;
-                case 2:
-                    myEventType = "Information";
-                    break;
-                case 3:
-                    myEventType = "Warning";
-                    break;
-                default: break;
-            }
-
-            EventLogEntryCollection myLogEntryCollection = myEventLog.Entries;
-            int myCount = myLogEntryCollection.Count;
-            // Iterate through all 'EventLogEntry' instances in 'EventLog'.
-            for (int i = myCount - 1; i > -1; i--)
-            {
-                EventLogEntry myLogEntry = myLogEntryCollection[i];
-                // Select the entry having desired EventType.
-                if (myLogEntry.EntryType.ToString().Equals(myEventType))
-                {
-                    // Display Source of the event.
-                    Console.WriteLine(myLogEntry.Source
-                       + " was the source of last event of type "
-                       + myLogEntry.EntryType);
-                    if (myLogEntry.Source == "Resource-Exhaustion-Detector" && (timeOfLastRestart.AddHours(1) > DateTime.Now))
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
         }
 
         private static bool IsServer_up()
